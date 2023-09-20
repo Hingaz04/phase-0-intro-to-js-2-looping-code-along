ea2a35d98608853a3558cf63aa9d2c84b06cc58d
Hingaz <hhinga04@gmail.com>
2023-09-20 18:57:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function countDown(enterednumber) {
+  if (enterednumber <= 0 || !Number.isInteger(enteredumber)) {
+    console.log("Please provide a positive integer as input.");
+    return;
+  }
+ while (enterednumber >= 0) {
+    console.log(enterednumber);
+    enterednumber--;
+  }
+}
+
+
+
+
+
